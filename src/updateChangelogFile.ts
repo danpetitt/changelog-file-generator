2f0b948c5897ac30d105f2a705f39b9e0084b104
Dan Petitt <danp@coderanger.com>
2020-10-20 21:18:59 +0100

    Added some logging

--- src/updateChangelogFile.ts
+++ src/updateChangelogFile.ts
@@ -6,7 +6,7 @@ export async function updateChangelogFile(
   changeLogPath: string,
   changeLog: string,
 ): Promise<void> {
-  info(`Updating changelog file at ${changeLogPath}`);
+  info(`Updating changelog file at '${changeLogPath}'`);
 
   const title = `# ${context.repo.repo}`;
 
@@ -14,11 +14,10 @@ export async function updateChangelogFile(
   if (section.length === 0) section = `## Release ${process.env.GITHUB_REF}`;
 
   let existingContent = '';
-  fs.access(changeLogPath, fs.constants.F_OK, async (err) => {
-    if (!err) {
-      existingContent = (await fs.promises.readFile(changeLogPath)).toString();
-    }
-  });
+  if (fs.existsSync(changeLogPath)) {
+    info('File exists, getting content');
+    existingContent = (await fs.promises.readFile(changeLogPath)).toString();
+  }
 
   const updatedContent = createNewContent(
     existingContent,
@@ -39,14 +38,18 @@ function createNewContent(
 ): string {
   let updatedContent = '';
   if (existingContent.length === 0) {
+    info(`Creating new changelog with title '${title}'`);
     updatedContent = `${title}\n\n${addNewReleaseSection(section, newContent)}`;
   } else {
+    info('Updating existing changelog');
+
     const releaseSection = addNewReleaseSection(section, newContent);
 
     // Find last release heading which will be a level 2 head
     const lastReleaseIndex = existingContent.indexOf('\n## ');
     if (lastReleaseIndex === -1) {
       // Should never get here really, but if we do append the new changelog to the end
+      // as its probably empty
       updatedContent = `${existingContent}\n\n${releaseSection}`;
     } else {
       updatedContent = `${existingContent
